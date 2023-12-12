31a86cb456e7b64b934a435a37c8a467c9041c86
geoniesun <leoniesonntag26@gmail.com>
2023-12-12 11:25:56 +0100

    my message

---
+++ R/myfirstpackagefile.R
@@ -0,0 +1,10 @@
+#to use functions you need to add the following line at the top of the other script
+#source("C:/eagle/wurzonie/collections/function_collection/veg_index.R")
+
+install.packages("roxygen2")
+library(roxygen2)
+roxygen2::roxygenise()
+
+NDWI_L8 <- function(gr, swir1){(gr-swir1)/(green+swir1)}
+
+NDVI <- function(red, nir){(red-nir)/(red+nir)}
